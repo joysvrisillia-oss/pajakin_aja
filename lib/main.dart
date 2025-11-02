ad3c06afe888612f5b455e985edc3fb8cda4de94
syarahh <cintyasyarahsyarief@gmail.com>
2025-11-02 20:24:23 +0700

    Resolve merge conflict in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 import 'Home_Page.dart';
 import 'Kalkulator_Pajak.dart';
@@ -6,13 +5,12 @@ import 'Riwayat_Perhitungan.dart';
 import 'Material.dart';
 
 
-=======
+
 
 import 'package:flutter/material.dart';
 import 'Home_Page.dart';
 import 'Material.dart';
 
->>>>>>> 6c1cc143556c4c7dadd7d75c38a5dbdaf6d09701
 void main() {
   runApp(const PajakinApp());
 }
